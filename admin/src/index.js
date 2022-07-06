8a078b66b9c1a42dc8e57e3b411da9694e60fa08
Himanshu Sharma <Himanshu.Sharma@AD.JKTECH.COM>
2022-07-06 16:01:05 +0530

    adding express & routes folder

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,1 +1,11 @@
-console.log('basic setup');
+const express=require('express');
+const AdminRoutes=require('./routes/admin-routes');
+const app=express();
+app.use(AdminRoutes);
+app.listen(3000,()=>{
+    console.log(`app listen to 3000  port`);
+})
+.on('error', (err) => {
+    console.log(err);
+    process.exit();
+});
