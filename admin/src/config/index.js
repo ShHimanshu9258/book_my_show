70bd7d581a6e7abebc00f15df98373ee59d2f931
Himanshu Sharma <Himanshu.Sharma@AD.JKTECH.COM>
2022-07-06 16:56:08 +0530

    connecting to db

--- admin/src/config/index.js
+++
@@ -1,8 +0,0 @@
-const dotenv=require('dotenv').config();
-console.log(process.env.PORT);
-module.exports={
-    PORT:process.env.PORT,
-    DB_URL:process.env.MONGODB_URL,
-    APP_SECRET:process.env.APP_SECRET
-    
-}
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,20 +1,32 @@
 const express=require('express');
 const bodyParser=require('body-parser');
-
+const mongoose=require('mongoose');
+const dotenv=require('dotenv').config();
 // importing routes
 const AdminRoutes=require('./routes/admin-routes');
-const { PORT } = require('./config');
+
 const app=express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// global variables decleration
+const PORT=`${process.env.PORT}`
+const MONGODB_URI =
+`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.qnaxe.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
 
 app.use(AdminRoutes);
-app.listen(PORT,()=>{
-    console.log(`app listen to 3000  port`);
-})
-.on('error', (err) => {
+ mongoose.connect(MONGODB_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log('connected to db');
+        console.log(`app listen to ${PORT}  port`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    });
+ }).catch(err=>{
     console.log(err);
-    process.exit();
-});
+ })
+
