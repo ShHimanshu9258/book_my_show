cc3b185f63899d98dab7b82b81feedc8de882943
Himanshu Sharma <Himanshu.Sharma@AD.JKTECH.COM>
2022-07-07 14:15:47 +0530

    basic setup for user

--- admin/src/routes/venue-routes.js
+++ admin/src/routes/venue-routes.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-const {VenderSignIn, GetVenueAdminProfileById, UpdateEventTiming, CancelEvent, PostponeEvent}=require('../controller/venue-controller');
+const {VenderSignIn, GetVenueAdminProfileById, UpdateEventTiming, CancelEvent, PostponeEvent, UpdateEventseats}=require('../controller/venue-controller');
 
 const isAuth=require('../middleware/is-auth');
 const isVenueAdmin=require('../middleware/is-venueAdmin');
@@ -15,6 +15,6 @@ router.patch('/updatevenuetiming/:id',isAuth,isVenueAdmin,UpdateEventTiming);
 router.patch('/cancel-event/:id',isAuth,isVenueAdmin,CancelEvent);
 
 router.patch('/postpone-event/:id',isAuth,isVenueAdmin,PostponeEvent);
-
+router.patch('/manageSeats-byvenueadmin/:id',isAuth,isVenueAdmin,UpdateEventseats);
 
 module.exports=router;
