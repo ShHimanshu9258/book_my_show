f5ff5b20c2aa40c6d05b8c5afcdc2f19c6c1b911
Himanshu Sharma <Himanshu.Sharma@AD.JKTECH.COM>
2022-07-07 15:22:53 +0530

    adding database connection

--- user/src/index.js
+++ user/src/index.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv').config();
 const app=express();
 
 // importing routes
@@ -9,8 +11,26 @@ const UserRoutes=require('./routes/user-routes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// global variables decleration
+const PORT=`${process.env.PORT}`
+const MONGODB_URI =
+`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.qnaxe.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
 
 // using routes
 app.use(UserRoutes);
 
-app.listen(4000);
+
+//db connection
+mongoose.connect(MONGODB_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log('connected to db');
+        console.log(`app listen to ${PORT}  port`);
+    })
+    .on('error', (err) => {
+        console.log(err);
+        process.exit();
+    });
+ }).catch(err=>{
+    console.log(err);
+ })
