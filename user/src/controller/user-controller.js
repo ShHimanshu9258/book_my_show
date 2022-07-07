22bc6f9bd28b43b388695a9763c65a0d3858c299
Himanshu Sharma <Himanshu.Sharma@AD.JKTECH.COM>
2022-07-07 15:40:45 +0530

    adding hashing

--- user/src/controller/user-controller.js
+++ user/src/controller/user-controller.js
@@ -1,5 +1,5 @@
 const User=require('../models/user');
-
+const {GeneratePassword,GenerateSalt,GenerateSignature, GetDataByEmail}=require('../utility');
 module.exports.GetUsers=async(req,res,next)=>{
     try{
         return res.json({message:'express api testing'});
@@ -12,16 +12,27 @@ module.exports.GetUsers=async(req,res,next)=>{
 module.exports.CreteUser=async(req,res,next)=>{
     try{
          const {email,password,name,phone}= req.body;
-         const user=new User({
-            email:email,
-            phone:phone,
-            address:[],
-            name:name,
-            password:password,
-            salt:'somesalt'
-         });
-         const result=await user.save();
-         return res.status(201).json(result);
+         const existingUser=await GetDataByEmail(email,User);
+         if(!existingUser){
+            const salt=await GenerateSalt();
+            const userPassword=await GeneratePassword(password,salt);
+            const user=new User({
+                email:email,
+                phone:phone,
+                address:[],
+                name:name,
+                password:userPassword,
+                salt:salt
+             });
+             const result=await user.save();
+             if(result){
+                return res.status(201).json(result);
+             }
+             return res.status(422).json({message:'No user added inside database'});
+             
+         }
+         return res.status(422).json({message:'user is already exist with this email'});
+         
     }
     catch(error){
         console.log(error);
